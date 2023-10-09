bfc1064c9d5e7bd164f060cc736e99c0c99bdcfb
Kuenzang Rabten <02230289.cst@rub.edu.bt>
2023-10-09 16:08:00 +0600

    task automation

---
+++ SWE_KINZANG RABTEN_02230289.py
@@ -0,0 +1,13 @@
+from instabot import Bot
+bot = Bot()
+
+bot.login(username="rabten1234", password="chinkey")
+
+target_username = "nam_huk708"
+bot.follow(target_username)
+
+bot.send_message("What's up brodas!",['namgay_2004','ronal_d_messi710'])
+
+bot.upload_photo("ronaldo.jpeg", caption="The GOAT")
+
+bot.logout() 
